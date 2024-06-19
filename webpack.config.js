d53d66c19618e7da36b2d30e1327fb200011f41d
kim <perfumeplaylist.gmail.com>
2024-06-19 17:54:12 +0900

    feat: babel loader 활용하여 js 코드 변경해주는 webpack 초기 설정

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/render.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    port: 9000,
+    hot: true,
+  },
+};
